94217fed44a61c94ec2e40b09a68fb4e7b3982af
Robert Haritonov <r@rhr.me>
2015-11-17 01:27:42 +0100

    check if enabled before building webpack

--- core/index.js
+++ core/index.js
@@ -2,17 +2,25 @@ var express = require('express');
 var webpack = require('webpack');
 var makeWebpackConfig = require('../src/make-webpack-config.js');
 var env = global.MODE || process.env.NODE_ENV;
-var compiler = webpack(makeWebpackConfig(env));
+var config = require('../src/utils/config');
 
-if (env === 'development') {
-	global.app.use(require('webpack-dev-middleware')(compiler, {
-		noInfo: true
-	}));
-	global.app.use(require('webpack-hot-middleware')(compiler));
-} else {
-	webpack(makeWebpackConfig('production'), function(err, stats) {
-		if (err) {
-			console.log(err, stats);
-		}
-	});
+if (config.enabled) {
+	var compiler = webpack(makeWebpackConfig(env));
+
+	if (env === 'development') {
+		global.app.use(require('webpack-dev-middleware')(compiler, {
+			noInfo: true
+		}));
+		global.app.use(require('webpack-hot-middleware')(compiler));
+	} else {
+		console.log('webpack building production styleguidist bundle...');
+
+		webpack(makeWebpackConfig('production'), function(err, stats) {
+			if (err) {
+				console.log(err, stats);
+			}
+
+			console.log('webpack production styleguidist build done');
+		});
+	}
 }
